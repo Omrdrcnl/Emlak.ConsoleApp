885b36fa5a45074c01de690854373265b497d093
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-06-23 12:16:04 +0300

    EmlakConsoleApp

---
+++ Emlak.ConsoleApp/Emlak.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Emlak.ConsoleApp
+{
+    public class Emlak
+    {
+        public Adres Adres;
+        public string IlanTarihi;
+        public Musteri Sahibi;
+        public decimal M2;
+        private VergiDahil fiyat;
+
+        public decimal FiyatVeri {
+            get { return fiyat.GetFiyat(); }
+            set { fiyat.SetFiyat(value); } 
+                             }
+        public Emlak() {
+        fiyat= new VergiDahil();
+        }
+    }
+    public class VergiDahil
+    {
+        private decimal fiyat;
+
+        public decimal GetFiyat()
+        {
+            decimal VergiOranı = 0.18m;
+            decimal VergiliFiyat = fiyat + (fiyat * VergiOranı);
+            return VergiliFiyat;
+        }
+        public void SetFiyat(decimal value)
+                    {
+            if(value <= 0)
+        {
+            throw new ArgumentException("Fiyat 0 veya Daha küçük olamaz");
+        }
+            else
+        {
+            fiyat = value;
+        }
+        }
+    }
+
+    public class Konut : Emlak
+    {
+        public int Odasayisi;
+        public string Cephe;
+    }
+    public class Arsa : Emlak
+    {
+        public double Alan;
+        public string ImarDurumu;
+    }
+    public class Villa : Konut
+    {
+        public decimal BahceAlanı;
+        public bool HavuzluMu;
+    }
+
+    public class SatilikVilla :Villa
+    {
+        public string YapiDurumu;
+    }
+    public class KiralikVilla :Villa
+    {
+        public bool EsyaLiMi;
+    }
+    public class Ev : Konut
+    {
+        public int Kat;
+        public int KatSayisi;
+        public bool SahibindenMi;
+    }
+    public class SatilikEv : Ev
+    {
+        public string yapiDurumu;
+    }
+    public class KiralikEv : Ev
+    {
+        public bool EsyaliMi;
+    }
+    public class SatilikArsa : Arsa
+    {
+        public bool TapuVarMi;
+    }
+    public class KiralikArsa : Arsa
+    {
+        public int KiraSüresi;
+    }
+    public class Musteri
+    {
+       
+        public string Ad;
+        public int Yas;
+        public Adres Adres;
+    }
+
+    public class Adres
+    {
+        public string Sehir;
+        public string Semt;
+        public string Sokak;
+        public string Ilce;
+        public int no;
+    }
+
+
+}
---
+++ Emlak.ConsoleApp/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace Emlak.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Emlak nesnesi oluşturma
+            Emlak emlak = new Emlak();
+
+            // Adres bilgilerini ayarlama
+            emlak.Adres = new Adres()
+            {
+                Sehir = "Çorum",
+                Semt = "Zafer",
+                Sokak = "Kara Sokak",
+                Ilce = "Alaca",
+                no = 123
+            };
+
+            
+           
+            // Konut nesnesi oluşturma
+            Konut konut = new Konut();
+            konut.Adres = new Adres()
+            {
+                Sehir = "İstanbul",
+                Semt = "Kadıköy",
+                Sokak = "Örnek Sokak",
+                Ilce = "Örnek İlçe",
+                no = 789
+            };
+
+          
+
+            konut.FiyatVeri = 200000;
+            konut.IlanTarihi = DateTime.Now.ToString("dd.MM.yyyy");
+            konut.Sahibi = new Musteri()
+            {
+               
+                Ad = "Ayşe Karagöz",
+                Yas = 35,
+                Adres = new Adres()
+                {
+                    Sehir = "İstanbul",
+                    Semt = "Çamlıdağ",
+                    Sokak = "Karanfil Sokak",
+                    Ilce = "Kadıköy",
+                    no = 321
+                }
+            };
+            konut.M2 = 120;
+            konut.Odasayisi = 3;
+            konut.Cephe = "Güney";
+
+            // Konut özelliklerini ekrana yazdırma
+            Console.WriteLine("\nSatılık Ev İlanı");
+            Console.WriteLine("Adres: {0}", GetAdresString(konut.Adres));
+            Console.WriteLine("Fiyat: {0}", konut.FiyatVeri);
+            Console.WriteLine("İlan Tarihi: {0}", konut.IlanTarihi);
+            Console.WriteLine("Sahibi: {0}", GetMusteriString(konut.Sahibi));
+            Console.WriteLine("Metrekare: {0}", konut.M2);
+            Console.WriteLine("Oda Sayısı: {0}", konut.Odasayisi);
+            Console.WriteLine("Cephe: {0}", konut.Cephe);
+
+            //Arsa Nesnesi Oluşturma
+            Arsa arsa = new Arsa()
+            {
+                Alan = 250,
+                IlanTarihi = "10.11.2022",
+                ImarDurumu = "İmarlı",
+                M2 = 250,
+                FiyatVeri= 300000,
+
+
+            };
+            arsa.Adres = new Adres()
+            {
+                Sehir = "Ankara",
+                Ilce = "Mamak",
+                Semt = "Yukarı Ayrancılar",
+                Sokak = "Daşdelen Sokak",
+                no = 89
+            };
+            arsa.Sahibi = new Musteri()
+            {
+                Ad = "Haşmet Kara",
+                Yas = 50,
+                Adres = new Adres()
+                {
+                    Sehir = "Nigde",
+                    Ilce = "Bor",
+                    Semt = "Kırkbayır",
+                    Sokak = "Dikili sokak",
+                    no =40
+                }
+                
+            };
+
+            //Arsa nesnesini ekrana yazdırma
+            Console.WriteLine("\nSatılık Arsa İlanı");
+            Console.WriteLine("Arsa Adresi: {0}", GetAdresString(arsa.Adres));
+            Console.WriteLine("Fiyat: {0}", arsa.FiyatVeri);
+            Console.WriteLine("İlan Tarihi: {0}", arsa.IlanTarihi);
+            Console.WriteLine("Sahibi: {0}", GetMusteriString(arsa.Sahibi));
+            Console.WriteLine("Metrekare: {0}", arsa.Alan);
+            
+            Console.WriteLine("Arsa İmar Durumu:", arsa.ImarDurumu);
+            
+
+            // Kullanıcıdan fiyatı alarak emlak.FiyatVeri'yi ayarlama
+            Console.WriteLine("\nLütfen Kiralık ev ilanı için Tutar giriniz: ");
+            decimal fiyat = decimal.Parse(Console.ReadLine());
+            emlak.FiyatVeri = fiyat;
+
+
+            // İlan tarihini ayarlama
+            emlak.IlanTarihi = DateTime.Now.ToString("dd.MM.yyyy");
+
+            // Sahibi ayarlama
+            emlak.Sahibi = new Musteri()
+            {
+
+                Ad = "Ahmet",
+                Yas = 30,
+                Adres = new Adres()
+                {
+                    Sehir = "Ankara",
+                    Semt = "Dutluk",
+                    Sokak = "yeşil Sokak",
+                    Ilce = "Çankaya",
+                    no = 456
+                }
+            };
+
+            // Metrekareyi ayarlama
+            emlak.M2 = 150;
+
+            // Emlak özelliklerini ekrana yazdırma
+            Console.WriteLine("Emlak Bilgileri");
+            Console.WriteLine("Adres: {0}", GetAdresString(emlak.Adres));
+            Console.WriteLine("Fiyat: {0}", emlak.FiyatVeri);
+            Console.WriteLine("İlan Tarihi: {0}", emlak.IlanTarihi);
+            Console.WriteLine("Sahibi: {0}", GetMusteriString(emlak.Sahibi));
+            Console.WriteLine("Metrekare: {0}", emlak.M2);
+
+
+
+            Console.ReadLine();
+        }
+            
+
+
+
+        private static string GetAdresString(Adres adres)
+        {
+            return string.Format("{0} {1} {2} Mahallesi {3} No:{4}", adres.Sehir, adres.Ilce, adres.Semt,  adres.Sokak, adres.no);
+        }
+
+        private static string GetMusteriString(Musteri musteri)
+        {
+            return string.Format(" Ad Soyad: {0}, Yaş: {1}, Adres Bilgisi: {2}",  musteri.Ad, musteri.Yas, GetAdresString(musteri.Adres));
+        }
+        
+    }   
+}
